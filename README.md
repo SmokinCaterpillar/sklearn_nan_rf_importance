6bd41bb346d0faab3076020aef036446d7107953
Robert Meyer <robert.meyer@alcemy.tech>
2019-10-07 21:11:25 +0200

    replication of the bug

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # sklearn_nan_rf_importance
 A github repo that contains a code snippet creating nan feature importances.
+
+The faulty feature importances may be caused by an already faulty
+node impurity which is NaN. See bug.py for a small code
+snippet that reproduces the code.
---
+++ bug.py
@@ -0,0 +1,20 @@
+import joblib
+import numpy as np
+
+rf = joblib.load('faulty_forest.pkl')
+X = joblib.load('faulty_X.pkl')
+y = joblib.load('faulty_y.pkl')
+sample_weight = joblib.load('faulty_sample_weight.pkl')
+
+rf.fit(X, y, sample_weight=sample_weight)
+
+print('Feature importances of the forest', rf.feature_importances_)
+
+faulty_trees = [t for t in rf.estimators_ if np.any(np.isnan(t.feature_importances_))]
+
+print('Number of trees with NaN importances:', len(faulty_trees))
+print('Feature importances of the faulty tree',
+      faulty_trees[0].feature_importances_)
+
+print('Potnetial cause is impurity 103 with is already: ',
+      faulty_trees[0].tree_.impurity[103])
